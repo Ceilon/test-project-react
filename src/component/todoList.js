d59b2b5df55a6fc491437f640d9522f15b9ef333
chailong <chailongyes@qq.com>
2019-08-03 11:20:17 +0800

    初始化

---
+++ src/component/todoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Button} from 'antd'
+
+export default class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <Button>
+                按钮
+            </Button>
+        )
+    }
+}
